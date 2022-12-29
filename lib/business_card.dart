8a35878c1691f9cc9a1f546cd79bdc51fbbcbd50
Yury Morozov <morozovypa1@gmail.com>
2022-12-30 03:59:40 +0800

    some code fix

--- lib/business_card.dart
+++ lib/business_card.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:rive/rive.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
 import 'constants.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'welcome',
       theme: ThemeData(
         scaffoldBackgroundColor: const Color(0xFF5C4D3E),
         primarySwatch: Colors.blue,
